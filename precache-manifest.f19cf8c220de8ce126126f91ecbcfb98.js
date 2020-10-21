7acfcded497367d54eb677bf7b1303f1c4d354c3
bs <bsandru@gmail.com>
2020-10-21 21:10:58 +0300

    Updates

---
+++ precache-manifest.f19cf8c220de8ce126126f91ecbcfb98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae22949cf39c00c852aae03509bb4f4",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "8676df77027412a85afd",
+    "url": "/react-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb8a5ca5e75d3b87024d",
+    "url": "/react-demo/static/js/2.23b63e60.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react-demo/static/js/2.23b63e60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8676df77027412a85afd",
+    "url": "/react-demo/static/js/main.b1aa5f14.chunk.js"
+  },
+  {
+    "revision": "c63e00b457bbb567fb4d",
+    "url": "/react-demo/static/js/runtime-main.31548a7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
